221ac37ad61eff232424f0d300f298ea133015e5
DTC174 <a01672034597@gmail.com>
2025-04-03 16:48:21 +0700

    Hoàn thành bài tập Exam-Deha

---
+++ bai4.py
@@ -0,0 +1,13 @@
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    return dp[n]
+
+n = int(input("Nhập số nguyên n: "))
+print(f"Số Fibonacci thứ {n} là: {fibonacci(n)}")
